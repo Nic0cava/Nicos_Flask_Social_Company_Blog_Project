c918211c8f2e68111937893c01334ff7e20c12e7
Nico <nmcava01@gmail.com>
2025-07-03 13:26:08 -0400

    finished main backend functionality

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -58,8 +58,10 @@ from companyblog import models # --> Must be placed here after Initialization
 
 from companyblog.core.views import core
 from companyblog.users.views import users
+from companyblog.blog_posts.views import blog_posts
 from companyblog.error_pages.handlers import error_pages
 app.register_blueprint(core)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
 app.register_blueprint(error_pages)
 
